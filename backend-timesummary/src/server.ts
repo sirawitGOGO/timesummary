43af800f38086e602a4f4f969aac4c9f74dc5a09
Sirawit Arsanok <beforever369@gmail.com>
2025-03-26 01:58:33 +0700

    update for deploy

--- backend-timesummary/src/server.ts
+++ backend-timesummary/src/server.ts
@@ -42,5 +42,5 @@ if (!databaseUrl) {
 // ใช้ routes
 app.use("/api/timesheet", timesheetRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = parseInt(process.env.PORT || "5000", 10);
 app.listen(PORT, "0.0.0.0", () => console.log(`Server running on port ${PORT}`));
